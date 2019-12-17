1fa68969f6474abde204b734c73da2fe57c23eb5
rizqito <qito25@gmail.com>
2019-12-17 13:21:58 +0800

    templating admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,14 +1,109 @@
 @extends('admin.layout.frame')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+<div class="container-fluid">
+    <div class="page_header">
+        <div class="left">
+            <h1>Dashboard</h1>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index-2.html"><i class="fa fa-home"></i></a></li>
+                <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+        </div>
+    </div>
+    <div class="mt-3"></div>
 
-                <div class="panel-body">
-                    You are logged in as Admin!
+    <div class="row clearfix">
+        <div class="col-xl-3 col-lg-3 col-md-6">
+            <div class="card state_w1">
+                <div class="body d-flex justify-content-between">
+                    <div>
+                        <h5>2,365</h5>
+                        <span><i class="fa fa-eye"></i> Post View</span>
+                    </div>
+                    <span class="sparkbar-small">10,8,9,3,5,8,5</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-3 col-md-6">
+            <div class="card state_w1">
+                <div class="body d-flex justify-content-between">
+                    <div>                                
+                        <h5>365</h5>
+                        <span><i class="fa fa-thumbs-up"></i> Likes</span>
+                    </div>
+                    <span class="sparkbar-small">8,5,2,2,1,6,4,8</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-3 col-md-6">
+            <div class="card state_w1">
+                <div class="body d-flex justify-content-between">
+                    <div>
+                        <h5>65</h5>
+                        <span><i class="fa fa-comment"></i> Comments</span>
+                    </div>
+                    <span class="sparkbar-small">10,8,9,3,5,8,5</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-lg-3 col-md-6">
+            <div class="card state_w1">
+                <div class="body d-flex justify-content-between">
+                    <div>                            
+                        <h5>2,055</h5>
+                        <span><i class="fa fa-user"></i> Profile Views</span>
+                    </div>
+                    <span class="sparkbar-small">6,3,8,5,1,2,4,9</span>
+                </div>
+            </div>
+        </div>
+    </div>            
+    
+    <div class="row clearfix">
+        <div class="col-xl-12 col-lg-12 col-md-12">
+            <div class="card">
+                <div class="header">
+                    <h2>Activities</h2>
+                    <ul class="header-dropdown">
+                        <li class="dropdown"> <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="zmdi zmdi-more"></i> </a>
+                            <ul class="dropdown-menu dropdown-menu-right slideUp">
+                                <li><a href="javascript:void(0);">Edit</a></li>
+                                <li><a href="javascript:void(0);">Delete</a></li>
+                                <li><a href="javascript:void(0);">Report</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="body">
+                    <ul class="list-unstyled activity">
+                        <li class="a_birthday">
+                            <h4>Admin Birthday</h4>
+                            <p>Lorem Ipsum is simply dummy text of the printing</p>
+                            <small>1 months ago.</small>
+                        </li>
+                        <li class="a_code">
+                            <h4>Code Change</h4>
+                            <p>It is a long established fact that a reader will be distracted</p>
+                            <small>1 week ago.</small>
+                        </li>
+                        <li class="a_contact">
+                            <h4>Add New Contact</h4>
+                            <code>maryamamiri@gmail.com</code>
+                            <code>fideltonn@gmail.com</code>
+                            <small>1 months ago.</small>
+                        </li>
+                        <li class="a_email">
+                            <h4>New Email</h4>
+                            <p>Contrary to popular belief, Lorem Ipsum is not simply</p>
+                            <small>3 months ago.</small>
+                        </li>
+                        <li class="a_contact">
+                            <h4>Add New Contact</h4>
+                            <code>maryamamiri@gmail.com</code>
+                            <small>1 months ago.</small>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
---
+++ resources/views/admin/layout/left-sidebar.blade.php
@@ -0,0 +1,37 @@
+<div class="left_sidebar">
+    <a class="brand-text" href="javascript:void(0);">ArrOwlite</a>
+    <ul class="navbar-nav mr-auto xs-show d-none">
+        <li class="nav-item xs-hide"><a class="nav-link nav-link-icon" href="search.html"><i class="fa fa-search"></i></a>
+        </li>
+        <li class="nav-item "><a class="nav-link nav-link-icon btn-dashboard active" href="javascript:void(0);"><i
+                    class="fa fa-dashboard"></i></a></li>
+        <li class="nav-item "><a class="nav-link nav-link-icon btn-ui-element" href="javascript:void(0);"><i
+                    class="fa fa-object-group"></i></a></li>
+        <li class="nav-item xs-hide"><a class="nav-link nav-link-icon btn-auth" href="javascript:void(0);"><i
+                    class="fa fa-lock"></i></a></li>
+    </ul>
+    <nav class="sidebar main_dashboard open">
+        <ul class="metismenu">
+            <li class="g_heading">-- Main</li>
+            <li class="active"><a href="index-2.html"><i class="ti-home"></i><span>Dashboard</span></a></li>
+            
+            <li class="g_heading">-- Application</li>
+            <li><a href="app-calendar.html"><i class="ti-calendar"></i><span>Calendar</span></a></li>
+            <li><a href="app-inbox.html"><i class="fa fa-inbox"></i><span>My Inbox</span></a></li>                
+            <li><a href="app-chat.html"><i class="ti-comments"></i><span>Chat Apps</span></a></li>
+            <li><a href="app-contact.html"><i class="ti-id-badge"></i><span>Contact</span></a></li>
+            <li><a href="app-taskboard.html"><i class="ti-notepad"></i>TaskBoard</a></li>
+            <li><a href="app-todo.html"><i class="fa fa-list-ul"></i><span>Todo List</span></a></li>
+            <li><a href="app-drive.html"><i class="fa fa-cloud"></i><span>My Drive</span></a></li>
+            <li class="g_heading">-- Extra</li>
+            <li><a href="app-widget.html"><i class="ti-widget"></i><span>Widgets</span></a></li>
+            <li><a href="page-timeline.html"><i class="ti-menu-alt"></i><span>Timeline</span></a></li>
+            <li><a href="page-gallery.html"><i class="fa fa-image"></i><span>Image Gallery</span></a></li>
+            <li><a href="page-invoices.html"><i class="ti-file"></i><span>Invoices</span></a></li>
+            <li><a href="page-faqs.html"><i class="fa fa-info-circle"></i><span>FAQs</span></a></li>
+            <li><a href="page-jvectormap.html"><i class="fa fa-map-o"></i><span>JVector Maps</span></a></li>
+            <li><a href="https://thememakker.com/templates/arrowlite/documentation/introduction.html"><i class="ti-file"></i>Documentation</a></li>
+            <li><a href="ui-elements.html"><i class="fa fa-heart"></i>UI Kit Elements</a></li>
+        </ul>            
+    </nav>
+</div>
